cc4b5a20abf4a3928f6fc9c6eb2ada2e02b8a4d3
zhou <zhouhaonan4@gmail.com>
2017-04-18 21:44:34 +0800

    add back component

--- src/pages/components/Back.js
+++ src/pages/components/Back.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
+import LeftArrow from 'react-icons/lib/fa/chevron-left'
+
+import './back.css'
+
 class Library extends Component {
+  back() {
+    console.log('back')
+    // console.log(this.props)
+    // console.log()
+    history.back()
+  }
   render() {
     return (
       <div className="back">
-        back
+        <div onClick={this.back.bind(this)}><LeftArrow/></div>
       </div>
     );
   }
