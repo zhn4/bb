d12b0b2a3ee49841135c4dd98d6b91de4bd15523
zhou <zhouhaonan4@gmail.com>
2017-04-21 18:18:48 +0800

    add back and add readinggroup page

---
+++ src/pages/passbook/Passbook0.js
@@ -0,0 +1,154 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+
+import Calendar from '../calendar/Calendar'
+
+// import GoHistory from 'react-icons/lib/go/history'
+
+let apiSwitch = require('../../apiSwitch')
+
+import './style/passbook.css'
+
+// let data = localStorage.getItem('userData') ? JSON.parse(localStorage.getItem('userData')) : []// 新建数组读取localStorage是否存在用户数据
+
+class Passbook extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isLogin: false,
+      consumeData: ''
+    }
+  }
+  judgeLogin() {
+    let data = localStorage.getItem('userData') ? JSON.parse(localStorage.getItem('userData')) : []
+    if(data.length > 0) {
+      this.setState({
+        isLogin: true
+      })
+    }
+  }
+  componentWillMount() {
+    // /api/tsgbooks/member_book_tracks/
+    fetch(apiSwitch() + '/api/tsgbooks/member_book_tracks/2017/4/?service_consume__member=1', {
+      mode: 'cors',
+      method: 'get',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+    })
+    .then(res => {
+      console.log(res)
+      if(res.ok) {
+        res.json()
+        .then(json => {
+          console.log(json)
+          this.setState({
+            consumeData: json[0].award
+          })
+          console.log(this.state.consumeData)
+        })
+      }else {
+        res.json()
+        .then(json => {
+          console.log("gg")
+        })
+      }
+    })
+    .catch(function(error) {
+      console.log('error', error)
+    })
+  }
+  componentDidMount() {
+    this.judgeLogin()
+  }
+  componentWillReceiveProps() {
+    this.judgeLogin()
+  }
+  handleAward() {
+    console.log('领取礼物，发ajax')
+  }
+  render() {
+    return (
+      <div className="passbook">
+        <div className="load-history">
+          <Link to="/history"></Link>
+        </div>
+
+        <Calendar/>
+
+        <div className="record">
+          <Award handleAward={this.handleAward.bind(this)}/>
+          <Tips/>
+          <Readingbooks/>
+          <LeadBtn isLogin={this.state.isLogin}/>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+// 是否满足条件领取奖励
+// 领取阅读奖励需要绑定onClick事件触发ajax
+// 领取后改变提示文字
+class Award extends Component {
+  render() {
+    return (
+      <div className="award">
+        <div onClick={this.props.handleAward}>领取阅读奖励</div>
+      </div>
+    )
+  }
+}
+
+// 借书次数
+// 时间
+// 多少本书（借书次数 x 3）
+class Tips extends Component {
+  render() {
+    return (
+      <div className="tips">
+        <div>第1次借书，好棒！</div>
+        <div>4月16日 ~ 4月30日</div>
+        <div>321</div>
+      </div>
+    )
+  }
+}
+
+// 数组数据：封面，书名
+class Readingbooks extends Component {
+  render() {
+    return (
+      <div className="reading-books">
+        <div>
+          <div>
+            <img src="" alt="cover"/>
+          </div>
+          <div>书名</div>
+        </div>
+      </div>
+    )
+  }
+}
+
+// 判断状态显示按钮
+// @params isLogin
+class LeadBtn extends Component {
+  render() {
+    return (
+      <div className="lead-btn">
+        {
+          this.props.isLogin
+          ?
+          <Link className="btn" to={"/readinggroup" + this.props.service_consume_id}>绘本阅读</Link>
+          :
+          <Link className="btn" to="/login">登陆</Link>
+        }
+      </div>
+    )
+  }
+}
+
+export default Passbook;
---
+++ src/pages/reading/ReadingGroup.js
@@ -0,0 +1,162 @@
+import React, { Component } from 'react';
+
+import Back from '../components/Back'
+
+import FaPlay from 'react-icons/lib/fa/play'
+import FaStar from 'react-icons/lib/fa/star'
+import FaPause from 'react-icons/lib/fa/pause'
+import FaShare from 'react-icons/lib/fa/share-square-o'
+
+let apiSwitch = require('../../apiSwitch')
+
+
+// let  apiSwitch = require('../../apiSwitch')
+
+import './style/reading.css'
+
+let showTime
+
+class ReadingGroup extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      bookInfo: [],
+      audioStatus: false,
+      audioCurrentTime: '00:00',
+      tags: []
+    }
+  }
+  componentWillMount() {
+    console.log(this.props.match.params.service_consume_id)
+    fetch(apiSwitch() + '/api/tsgbooks/books_by_service_consume/'+ this.props.match.params.service_consume_id, {
+      mode: 'cors',
+      method: 'get',
+      headers: {
+        'Accept': 'application/json',
+        // 'Content-Type': 'application/json'
+      },
+    })
+    .then(res => {
+      console.log(res)
+      if(res.ok) {
+        res.json()
+        .then(json => {
+          console.log(json)
+          this.setState({
+            bookInfo: json,
+            tags: json.tag
+          })
+          this.refs.videoPlayer.load()
+        })
+      }else {
+        res.json()
+        .then(json => {
+          console.log("gg")
+        })
+      }
+    })
+    .catch(function(error) {
+      console.log('error', error)
+    })
+  }
+  changeTime(times) {
+    // let time = parseInt(this.refs.videoPlayer.duration, 10)
+    let time = parseInt(times, 10)
+    //分钟
+    let minute = time / 60
+    let minutes = parseInt(minute, 10)
+    if (minutes < 10) {
+      minutes = "0" + minutes
+    }
+    //秒
+    let second = time % 60
+    let seconds = parseInt(second, 10)
+    if (seconds < 10) {
+      seconds = "0" + seconds
+    }
+    let allTime = '' +
+    minutes +
+    '' +
+    ':' +
+    '' +
+    seconds +
+    ''
+    // console.log(allTime)
+    return allTime
+  }
+  videoTime() {
+    showTime = setInterval( () => {
+      this.setState({
+        audioCurrentTime: this.changeTime(this.refs.videoPlayer.currentTime)
+      })
+    }, 1000)
+  }
+  playVideo() {
+    if(this.refs.videoPlayer.paused) {
+      this.refs.videoPlayer.play()
+      this.videoTime()
+      this.setState({
+        audioStatus: true
+      })
+    }else {
+      this.refs.videoPlayer.pause()
+      window.clearInterval(showTime)
+      this.setState({
+        audioStatus: false
+      })
+    }
+  }
+  audioEnd() {
+    console.log('end')
+    window.clearInterval(showTime)
+    this.setState({
+      audioStatus: false,
+      audioCurrentTime: '00:00'
+    })
+  }
+  render() {
+    return (
+      <div className="reading">
+        <Back/>
+        <div className="cover">
+          <img src={this.state.bookInfo.cover} alt="cover"/>
+          <p>{this.state.bookInfo.title}</p>
+        </div>
+        <div className="tags">
+          <ul>
+          {this.state.tags.map((data, i) => (
+            <li key={i}>{data.name}</li>
+          ))}
+          </ul>
+        </div>
+        <p className="desc">{this.state.bookInfo.content}</p>
+        <div>
+          <audio controls="controls" id="video-player" ref="videoPlayer" onEnded={this.audioEnd.bind(this)}>
+            <source src={this.state.bookInfo.audio} type="audio/mp3" />
+          </audio>
+        </div>
+        <div className="reading-nav">
+          <div>
+            <div><FaShare size={28}/></div>
+          </div>
+          <div className="active" data-value={this.state.audioCurrentTime} onClick={this.playVideo.bind(this)}>
+            <div>
+              {
+                this.state.audioStatus
+                ?
+                <FaPause size={28}/>
+                :
+                <FaPlay size={28}/>
+              }
+            </div>
+          </div>
+          <div className="unstar">
+            <div><FaStar size={28}/></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ReadingGroup;
