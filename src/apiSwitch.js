89d5d9bd015c6fd3114e935e604a103ce22206bc
zhou <zhouhaonan4@gmail.com>
2017-04-25 19:06:10 +0800

    modify

--- src/apiSwitch.js
+++ src/apiSwitch.js
@@ -1,8 +1,8 @@
 // 切换开发环境/生产环境的api
 module.exports = function apiSwitch() {
   if(process.env.NODE_ENV === 'development') {
-    return '//192.168.1.130:8000'
+    return 'http://192.168.1.130:8000'
   }else {
-    return '//192.168.1.130:8000'
+    return 'http://192.168.1.130:8000'
   }
 }
